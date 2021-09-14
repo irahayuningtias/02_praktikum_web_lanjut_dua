6cad19e2d5dda4b691a13c7fab6ca10e8a8f1245
irahayuningtias <2031710085@student.polinema.ac.id>
2021-09-14 13:34:04 +0700

    Controller Kedua

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo "NIM : 2031710085 <br>";
+        echo "Nama: Indah Rahayuningtias";
+}
